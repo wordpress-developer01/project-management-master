57362ff64921c75878bbe7c2abaa00d10f5b5909
wordpress-developer01 <130246057+wordpress-developer01@users.noreply.github.com>
2026-03-02 17:27:42 +0300

    Add root route

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,3 +38,7 @@ const port = Number(process.env.PORT) || 3000;
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on part ${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
