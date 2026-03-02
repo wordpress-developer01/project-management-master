31e26da3d2675b7c85da5b9b92e94da6d0077ae7
Muratbek Nassyrov <130246057+wordpress-developer01@users.noreply.github.com>
2026-03-02 18:19:23 +0300

    Change team name from 'EDROH' to 'MURAT'

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -82,7 +82,7 @@ const Sidebar = () => {
           />
           <div>
             <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
-              EDROH TEAM
+              MURAT TEAM
             </h3>
             <div className="mt-1 flex items-start gap-2">
               <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
